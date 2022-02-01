4624e4796dcb539309dd5f324cb2b6871e32ac1f
Marcus Sena <maarvins.19@gmail.com>
2022-02-01 20:54:20 -0300

    add: consuming api on the aplication

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import api from './services/api'
 
 function App() {
 
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({}) //estado que recebe RESPONSE.DATA (dados da api)
+
   async function handleSearch() {
     if (input === '') {
       alert ("Preencha o CEP!")
@@ -16,6 +19,8 @@ function App() {
 
     try {
       const response = await api.get(`${input}/json`)
+      setCep(response.data) //objeto de retorno com dados da API
+      setInput("")
 
     } catch {
       alert("Ops... Algo de errado não está certo! Certeza que estamos no mesmo planeta?  ")
@@ -23,7 +28,6 @@ function App() {
     }
   }
 
-  const [input, setInput] = useState('')
 
   return (
     <div className="container">
@@ -43,11 +47,12 @@ function App() {
      </div>
 
      <main className="main">
-       <h2>Cep: 0000000</h2>
-       <span>Rua: Carlos Antonio Giordane</span>
-       <span>Complemento: Cardan Sete</span>
-       <span>Bairro: Henrique Nery</span>
-       <span>Sete lagoas - MG</span>
+       <h2>Cep: {cep.cep}</h2>
+
+       <span>{cep.logradouro}</span>
+       <span>Complemento: {cep.complemento}</span>
+       <span>{cep.bairro}</span>
+       <span>{cep.localidade} -{cep.uf}</span>
      </main>
     </div>
   );
