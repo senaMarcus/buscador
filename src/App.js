a9d4e1278ce62b322e988bddf4e261964d2b77ca
Marcus Sena <maarvins.19@gmail.com>
2022-02-01 20:32:10 -0300

    add: using state from react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
+import {useState} from "react"
+
 import {FiSearch} from "react-icons/fi"
 import './styles.css'
 
 function App() {
+
+  function handleSearch() {
+    alert ("valor do input " + input)
+  }
+
+  const [input, setInput] = useState('')
+
   return (
     <div className="container">
      <h1 className="title">Buscador de CEP</h1>
      <div className="containerInput">
+
        <input type="text"
-       placeholder="Digite o CEP..." />
-       <button className="btnSearch">
+       placeholder="Digite o CEP..." 
+       value={input} //atrelado ao que for inserido no input
+       onChange={(e) => setInput(e.target.value)} //guarndando o valor do input
+       />
+
+       <button className="btnSearch" onClick={handleSearch}>
          <FiSearch size={25} color="#fff"/>
        </button>
+
      </div>
+
      <main className="main">
        <h2>Cep: 0000000</h2>
        <span>Rua: Carlos Antonio Giordane</span>
